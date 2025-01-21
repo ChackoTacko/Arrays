6cf43a7112c60610bd95a18ff7abfd56b4bcae38
Robert Lowe <rlowedevelopment@gmail.com>
2025-01-21 15:07:37 -0800

    Static and Dynamic Arrays with Operations and Time Complexity

---
+++ dynamic.js
@@ -0,0 +1,4 @@
+const dynamicArray = [];
+dynamicArray.push(1)
+
+console.log(dynamicArray)
---
+++ static.js
@@ -0,0 +1,10 @@
+let staticArray = new Array(7).fill(null);
+staticArray[0] = "Monday";
+staticArray[1] = "Tuesday";
+staticArray[2] = "Wednesday";
+staticArray[3] = "Thursday";
+staticArray[4] = "Friday";
+staticArray[5] = "Saturday";
+staticArray[6] = "Sunday";
+
+console.log(staticArray)
